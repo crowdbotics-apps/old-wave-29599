4a774b43952d920611d798c6c954c7f296c4b070
Crowdbotics <team@crowdbotics.com>
2021-08-07 10:01:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "old_wave_29599Identifier",
+  url: "https://old_wave_29599Identifier.botics.co",
+  api: "https://old_wave_29599Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
